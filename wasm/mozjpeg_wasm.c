9fa8c00395057b9fa5cd33c0773cd159d7ad94da
ForGoodTech <88217240+ForGoodTech@users.noreply.github.com>
2025-08-16 16:58:54 -0400

    Add detailed line trace logging in WASM wrapper

--- wasm/mozjpeg_wasm.c
+++ wasm/mozjpeg_wasm.c
@@ -29,78 +29,142 @@ int wasm_compress(const char *infilename, const char *outfilename,
   FILE *infile = NULL, *outfile = NULL;
   int retval = 0;
 
+  /* Trace macro to emit the current source line for debugging */
+#define TRACE_LINE()                                                      \
+  do {                                                                    \
+    fprintf(stderr, "TRACE %s:%d\n", __FILE__, __LINE__);               \
+    fflush(stderr);                                                       \
+  } while (0)
+
+  TRACE_LINE();
   fprintf(stderr, "wasm_compress: '%s' -> '%s' quality=%d\n", infilename,
           outfilename, quality);
   fflush(stderr);
 
+  TRACE_LINE();
   fprintf(stderr, "Loading input file...\n");
   infile = fopen(infilename, "rb");
+  TRACE_LINE();
   if (!infile) {
     perror(infilename);
+    TRACE_LINE();
     goto bailout;
   }
-  if (fseek(infile, 0, SEEK_END) < 0)
+  TRACE_LINE();
+  if (fseek(infile, 0, SEEK_END) < 0) {
+    TRACE_LINE();
     goto bailout;
+  }
+  TRACE_LINE();
   inSize = ftell(infile);
+  TRACE_LINE();
   rewind(infile);
+  TRACE_LINE();
   inBuf = (unsigned char *)tjAlloc(inSize);
-  if (!inBuf)
+  TRACE_LINE();
+  if (!inBuf) {
+    TRACE_LINE();
     goto bailout;
+  }
+  TRACE_LINE();
   if (fread(inBuf, 1, inSize, infile) != inSize) {
     perror(infilename);
+    TRACE_LINE();
     goto bailout;
   }
+  TRACE_LINE();
   fclose(infile);
+  TRACE_LINE();
   infile = NULL;
 
+  TRACE_LINE();
   dhandle = tjInitDecompress();
-  if (!dhandle)
+  TRACE_LINE();
+  if (!dhandle) {
+    TRACE_LINE();
     goto bailout;
-  if (tjDecompressHeader2(dhandle, inBuf, inSize, &width, &height, NULL) < 0)
+  }
+  TRACE_LINE();
+  if (tjDecompressHeader2(dhandle, inBuf, inSize, &width, &height, NULL) < 0) {
+    TRACE_LINE();
     goto bailout;
+  }
+  TRACE_LINE();
   rgbBuf = (unsigned char *)tjAlloc(width * height * tjPixelSize[TJPF_RGB]);
-  if (!rgbBuf)
+  TRACE_LINE();
+  if (!rgbBuf) {
+    TRACE_LINE();
     goto bailout;
+  }
+  TRACE_LINE();
   if (tjDecompress2(dhandle, inBuf, inSize, rgbBuf, width, 0, height,
-                    TJPF_RGB, 0) < 0)
+                    TJPF_RGB, 0) < 0) {
+    TRACE_LINE();
     goto bailout;
+  }
+  TRACE_LINE();
   tjDestroy(dhandle);
+  TRACE_LINE();
   dhandle = NULL;
+  TRACE_LINE();
   tjFree(inBuf);
+  TRACE_LINE();
   inBuf = NULL;
+  TRACE_LINE();
   fprintf(stderr, "Loaded image %dx%d\n", width, height);
   fflush(stderr);
 
+  TRACE_LINE();
   fprintf(stderr, "Initializing compressor...\n");
+  TRACE_LINE();
   chandle = tjInitCompress();
-  if (!chandle)
+  TRACE_LINE();
+  if (!chandle) {
+    TRACE_LINE();
     goto bailout;
+  }
 
+  TRACE_LINE();
   fprintf(stderr, "Compressing...\n");
+  TRACE_LINE();
   if (tjCompress2(chandle, rgbBuf, width, 0, height, TJPF_RGB, &jpegBuf,
-                  &jpegSize, TJSAMP_420, quality, TJFLAG_ACCURATEDCT) < 0)
+                  &jpegSize, TJSAMP_420, quality, TJFLAG_ACCURATEDCT) < 0) {
+    TRACE_LINE();
     goto bailout;
+  }
+  TRACE_LINE();
   fprintf(stderr, "Compression produced %lu bytes\n", jpegSize);
   fflush(stderr);
 
+  TRACE_LINE();
   fprintf(stderr, "Writing output file...\n");
+  TRACE_LINE();
   outfile = fopen(outfilename, "wb");
+  TRACE_LINE();
   if (!outfile) {
     perror(outfilename);
+    TRACE_LINE();
     goto bailout;
   }
+  TRACE_LINE();
   if (fwrite(jpegBuf, jpegSize, 1, outfile) != 1) {
     perror(outfilename);
+    TRACE_LINE();
     goto bailout;
   }
 
-  if (rate)
+  TRACE_LINE();
+  if (rate) {
     *rate = (double)jpegSize /
             (double)(width * height * tjPixelSize[TJPF_RGB]) * 8.0;
+    TRACE_LINE();
+  }
 
+  TRACE_LINE();
   retval = 1; /* Success */
 
 bailout:
+  TRACE_LINE();
   if (!retval) {
     const char *err = chandle
                           ? tjGetErrorStr2(chandle)
@@ -122,5 +186,8 @@ bailout:
     tjDestroy(dhandle);
   if (infile)
     fclose(infile);
+  TRACE_LINE();
   return retval;
 }
+
+#undef TRACE_LINE
